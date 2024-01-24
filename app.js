2617f4d6d4781aed435c94cd267ca9b140c7576d
Estefani Rodriguez <rodriguezpinedaestef@gmail.com>
2024-01-24 17:10:41 -0600

    Añadí el despliegue del valor de la variable

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-alert('Hola Mundo');
+//Variables
+let numeroSecreto = 6;
+let numeroUsuario = prompt('Me indicas un número entre 1 y 10 por favor');
+
+console.log(numeroUsuario);
+
+/*Este código realiza
+la compilación*/
+
+if (numeroUsuario == numeroSecreto) {
+    //Acertamos, fue verdadera la condición
+    alert(`Acertaste, el número es: ${numeroUsuario}`);
+} else {
+    //La condicion no se cumplio
+    alert('Lo siento, no acertaste el número')
+}
