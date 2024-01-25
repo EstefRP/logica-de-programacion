99e724e34d3516c6ecd233e3b962e5703bb84b15
Estefani Rodriguez <rodriguezpinedaestef@gmail.com>
2024-01-24 18:59:52 -0600

    agregue loop

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 //Variables
-let numeroSecreto = 6;
-let numeroUsuario = prompt('Me indicas un número entre 1 y 10 por favor');
+let numeroSecreto = 5;
+let numeroUsuario=0;
+while (numeroUsuario!= numeroSecreto) {
+    numeroUsuario = prompt('Me indicas un número entre 1 y 10 por favor');
 
-console.log(numeroUsuario);
+    console.log(numeroUsuario);
 
-/*Este código realiza
-la compilación*/
+    /*Este código realiza
+    la compilación*/
 
-if (numeroUsuario == numeroSecreto) {
-    //Acertamos, fue verdadera la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else {
-    //La condicion no se cumplio
-    alert('Lo siento, no acertaste el número')
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}`);
+    } else {
+        if(numeroUsuario > numeroSecreto){
+            alert('El número secreto es menor');
+        } else{
+            alert('El número secreto es mayor')
+        }
+        //La condicion no se cumplio
+        //alert('Lo siento, no acertaste el número')
+    }
 }
